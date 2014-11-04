3bf3c393c324c484cd4a11d638ce6d68b72e83d9
Fabio Maggio <fabio@infoweb.be>
2014-11-04 11:53:21 +0100

    Add translation messages

---
+++ messages/nl/zelenin/modules/i18n.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Category' => 'Categorie',
+    'ID' => 'ID',
+    'Language' => 'Taal',
+    'Message' => 'Bericht',
+    'Not translated' => 'Niet vertaald',
+    'Source message' => 'Origineel bericht',
+    'Translated' => 'Vertaald',
+    'Translation' => 'Vertaling',
+    'Translation status' => 'Status van de vertaling',
+    'Translations' => 'Vertalingen',
+    'Update' => 'Wijzigen',
+    'Updated' => 'Gewijzigd',
+];
